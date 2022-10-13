3b581ab9add0e94566b464529c44cda753a6d929
marcosMartinezSoftwareoneSantiago <Marcos.Martinez@Softwareone.com>
2022-10-13 17:04:12 +0200

    Not really much done

---
+++ Exercise 9/Exercise9.js
@@ -0,0 +1,2 @@
+"use strict";
+let formulario = document.querySelector('form');
---
+++ Exercise 9/Exercise9.ts
@@ -0,0 +1,5 @@
+let formulario =document.querySelector('form');
+let primerNumero =document.querySelector('#primer-numero');
+let SegundoNumero =document.querySelector('#segundo-numero');
+
+
